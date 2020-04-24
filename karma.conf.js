215485af925835942f0bbc0cd444c82f5cbc66b8
Hacene <hacene.upmc@gmail.com>
2020-04-24 14:48:53 +0200

    adding travis

--- karma.conf.js
+++ karma.conf.js
@@ -7,26 +7,24 @@ module.exports = function(config) {
         frameworks: ['jasmine', '@angular-devkit/build-angular'],
         plugins: [
             require('karma-jasmine'),
+
+            require('karma-coverage'),
             require('karma-chrome-launcher'),
             require('karma-jasmine-html-reporter'),
             require('karma-coverage-istanbul-reporter'),
-            require('@angular-devkit/build-angular/plugins/karma'),
-            require('karma-coverage')
+            require('@angular-devkit/build-angular/plugins/karma')
         ],
         reporters: [
+            // Output code coverage files
             'coverage'
         ],
         client: {
             clearContext: false // leave Jasmine Spec Runner output visible in browser
         },
-        // Configure code coverage reporter
-        coverageReporter: {
-            reporters: [
-                // generates ./coverage/lcov.info
-                { type: 'lcovonly', subdir: '.' },
-                // generates ./coverage/coverage-final.json
-                { type: 'json', subdir: '.' },
-            ]
+        coverageIstanbulReporter: {
+            dir: require('path').join(__dirname, './coverage/hacene-blog'),
+            reports: ['html', 'lcovonly', 'text-summary'],
+            fixWebpackSourcePaths: true
         },
         customLaunchers: {
             ChromeHeadless: {
